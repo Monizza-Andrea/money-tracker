e0bbbcf357d6458b4c0bb7f386ef6c90e2777b78
Amonizz <andrea.monizza@gmail.com>
2024-12-29 21:37:43 +0100

    crea il controller ed inserisce dei metodi per la risposte alle chiamate

--- src/main/java/com/example/money_tracker/controller/TransactionController.java
+++ src/main/java/com/example/money_tracker/controller/TransactionController.java
@@ -1,9 +1,14 @@
 package com.example.money_tracker.controller;
 
+import com.example.money_tracker.model.Transaction;
 import com.example.money_tracker.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -11,4 +16,29 @@ public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> addTransaction(@RequestBody Transaction transaction) {
+        return ResponseEntity.ok(this.transactionService.addTransaction(transaction));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTransaction(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(this.transactionService.findById(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Transazione non trovata");
+        }
+    }
+
+    @GetMapping("/total-income")
+    public ResponseEntity<BigDecimal> getTotalIncome(@RequestParam int year, @RequestParam int month) {
+        return ResponseEntity.ok(transactionService.getTotalIncome(year, month));
+    }
+
+    @GetMapping("/total-expense")
+    public ResponseEntity<BigDecimal> getTotalExpense(@RequestParam int year, @RequestParam int month) {
+        return ResponseEntity.ok(transactionService.getTotalExpense(year, month));
+    }
+
 }
